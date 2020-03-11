fe96534f9c4e887a48300ea6850d6f8f28d0d151
Gneric <obiondii99@gmail.com>
2020-03-11 09:03:27 -0500

    FInal

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=G:\Javascript\flutter-apps\flutter-sistemaTransporte"
+export "FLUTTER_APPLICATION_PATH=D:\Development\flutter\flutter-sistemaTransporte"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
